a9535b5d8c923557d1c340965f1e92450c444d41
AntonyOspino <tonny1998_thony@hotmail.com>
2023-03-22 12:41:30 -0500

    modificacion a fondo, span y boton

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const imagen1 = document.querySelector('#imagen1');
 const imagen2 = document.querySelector('#imagen2');
 const imagen3 = document.querySelector('#imagen3');
+const starbucks = document.querySelector('.starbucks');
+const fondoCirculo = document.querySelector('.circulo');
+const span = document.querySelector('.span');
+const ancla = document.querySelector('.ancla');
 
 imagen1.addEventListener("click", function() {
     imgSlider('./imagenes/img1.png');
@@ -17,5 +21,22 @@ imagen3.addEventListener("click", function() {
 
 function imgSlider(imagenEntrante){
     // Cambiar la fuente de la imagen grande por la de la imagen entrante
-    document.querySelector('.starbucks').src = imagenEntrante;
+    if(imagenEntrante == './imagenes/img1.png'){
+        starbucks.src = imagenEntrante;
+        fondoCirculo.style.backgroundColor = '#017143';
+        span.style.color = '#017143';
+        ancla.style.backgroundColor = '#017143';
+
+    }else if(imagenEntrante == './imagenes/img2.png'){
+        starbucks.src = imagenEntrante;
+        fondoCirculo.style.backgroundColor = '#e36783';
+        span.style.color = '#e36783';
+        ancla.style.backgroundColor = '#e36783';
+
+    }else{
+        starbucks.src = imagenEntrante;
+        fondoCirculo.style.backgroundColor = '#7b2061';
+        span.style.color = '#7b2061';
+        ancla.style.backgroundColor = '#7b2061';
+    }
 }
